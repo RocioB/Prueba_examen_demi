95fa269f8efac843659a6f4c74e0c329d7d1ac6d
RocioB <rociobarlo@gmail.com>
2014-12-17 00:05:55 +0100

    af

---
+++ Assets/scripts/GeneradorDeBalasScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorDeBalasScript : MonoBehaviour {
+	
+	
+	public GameObject bala;
+	public Transform puntoDisparo;
+	public AudioClip sound;
+	
+	public void Fire(){
+		if (bala != null) {
+			var clone = Instantiate (bala, puntoDisparo.position, Quaternion.identity) as GameObject;
+			clone.transform.localScale = transform.localScale;
+
+			
+		} else {
+			Debug.Log("El jugador no tiene balas");
+		}
+	}
+}
---
+++ Assets/scripts/cambioNivel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cambioNivel : MonoBehaviour {
+	public string namelevel;
+
+
+	public void pasarnivelpollas(){
+
+		Application.LoadLevel (namelevel);
+
+
+	}
+}
